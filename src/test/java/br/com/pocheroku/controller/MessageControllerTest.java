b0b219f22aea60acf49a28df30236f776c178e98
arthur_boeck <#Menezes5>
2022-03-01 14:15:26 -0300

    ajustes testes

--- src/test/java/br/com/pocheroku/controller/MessageControllerTest.java
+++ src/test/java/br/com/pocheroku/controller/MessageControllerTest.java
@@ -3,7 +3,9 @@ package br.com.pocheroku.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MessageController.class)
+@AutoConfigureMockMvc
 public class MessageControllerTest {
 
     @Autowired
@@ -22,7 +25,7 @@ public class MessageControllerTest {
     @Test
     public void checkHelloWorld() throws Exception {
         mockMvc
-                .perform(MockMvcRequestBuilders.get("/hello"))
+                .perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("message").value("Hello Dear World!"));
     }
