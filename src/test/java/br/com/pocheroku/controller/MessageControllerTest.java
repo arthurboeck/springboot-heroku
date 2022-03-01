ffec224119cce623fc7fba6394633588ca56a987
arthur_boeck <#Menezes5>
2022-03-01 16:12:09 -0300

    fix test

--- src/test/java/br/com/pocheroku/controller/MessageControllerTest.java
+++ src/test/java/br/com/pocheroku/controller/MessageControllerTest.java
@@ -1,12 +1,10 @@
 package br.com.pocheroku.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -14,7 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(MessageController.class)
 @AutoConfigureMockMvc
 public class MessageControllerTest {
